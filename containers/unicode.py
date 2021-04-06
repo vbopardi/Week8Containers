d14b722a917afaf9fd09adf0650d7d28c331f980
vbopardi <vbopard@gmail.com>
2021-04-06 12:32:45 -0700

    Unicode!

--- containers/unicode.py
+++ containers/unicode.py
@@ -12,7 +12,7 @@ class NormalizedStr:
     >>> 'César' in 'César Chávez'
     False
 
-    The two strings to the right of the in keyword above are equal *semantically*,
+    The two strings to the right of the in keyword above are equal
     but not equal *representationally*.
     In particular, the first is in NFC form, and the second is in NFD form.
     The purpose of this class is to automatically normalize our strings for us,
@@ -20,19 +20,28 @@ class NormalizedStr:
     '''
 
     def __init__(self, text, normal_form='NFC'):
+        self.text = unicodedata.normalize(normal_form, text)
+        self.normal_form = normal_form
         pass
 
     def __repr__(self):
         '''
-        The string returned by the __repr__ function should be valid python code
-        that can be substituted directly into the python interpreter to reproduce an equivalent object.
+        The string returned by the __repr__ function should be valid python
+        that can be substituted directly into the python interprete
         '''
+
+        t = str(self.text)
+        nf = str(self.normal_form)
+
+        return "NormalizedStr('" + t + "', '" + nf + "')"
 
     def __str__(self):
         '''
         This functions converts the NormalizedStr into a regular string object.
-        The output is similar, but not exactly the same, as the __repr__ function.
+        The output is similar, but not exactly the same, as the __repr__
         '''
+
+        return str(self.text)
 
     def __len__(self):
         '''
@@ -40,14 +49,21 @@ class NormalizedStr:
         The expression `len(a)` desugars to a.__len__().
         '''
 
+        return len(self.text)
+
     def __contains__(self, substr):
         '''
         Returns true if the `substr` variable is contained within `self`.
         The expression `a in b` desugars to `b.__contains__(a)`.
 
-        HINT:
-        You should normalize the `substr` variable to ensure that the comparison is done semantically and not syntactically.
         '''
+
+        nsubstr = unicodedata.normalize(self.normal_form, substr)
+
+        if nsubstr in self.text:
+            return True
+
+        return False
 
     def __getitem__(self, index):
         '''
@@ -55,15 +71,21 @@ class NormalizedStr:
         The expression `a[b]` desugars to `a.__getitem__(b)`.
         '''
 
+        return self.text[index]
+
     def lower(self):
         '''
         Returns a copy in the same normalized form, but lower case.
         '''
 
+        return str(self.text).lower()
+
     def upper(self):
         '''
         Returns a copy in the same normalized form, but upper case.
         '''
+
+        return str(self.text).upper()
 
     def __add__(self, b):
         '''
@@ -71,14 +93,30 @@ class NormalizedStr:
         The expression `a + b` gets desugared into `a.__add__(b)`.
 
         HINT:
-        The addition of two normalized strings is not guaranteed to stay normalized.
-        Therefore, you must renormalize the strings after adding them together.
         '''
+
+        nnb = self.text + str(b)
+        nnbn = unicodedata.normalize(self.normal_form, nnb)
+
+        return NormalizedStr(nnbn)
 
     def __iter__(self):
         '''
         HINT:
-        Recall that the __iter__ method returns a class, which is the iterator object.
-        You'll need to define your own iterator class with the appropriate magic methods,
-        and return an instance of that class here.
         '''
+
+        return Iterator(self.text)
+
+
+class Iterator:
+    def __init__(self, text):
+        self.text = text
+        self.ct = 0
+
+    def __next__(self):
+        if len(self.text) <= self.ct:
+            raise StopIteration
+        else:
+            idx = self.text[self.ct]
+            self.ct += 1
+            return idx
